00981d368c90a7c79c1c94695fa034dbc3493db1
lpotages <lpotages@gmail.com>
2016-03-11 09:40:47 +0100

    Correction

--- build_all.sh
+++ build_all.sh
@@ -6,11 +6,15 @@ mvn clean package
 cd ..
 
 ## Building the j2e system
-cd j2e
+cd j2e/core
 mvn clean package
 cd ..
 
+cd adecouper
+mvn clean package
+cd ../..
+
 ## Building the .Net system
-cd dotNet
-./compile.sh
-cd ..
+##cd dotNet
+##./compile.sh
+##cd ..
